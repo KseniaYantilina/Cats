22ee0796a27315968f5ed293a57c0b71a2f6671f
KseniaYantilina <walxenia@yandex.ru>
2025-07-31 00:30:13 +0500

    Добавлена кнопка для загрузки нового изображения

--- Cataas.py
+++ Cataas.py
@@ -6,7 +6,7 @@ import requests
 from io import BytesIO
 
 
-def load_image():
+def load_image(url):
     try:
         response = requests.get(url)
         response.raise_for_status()
@@ -17,6 +17,14 @@ def load_image():
         print(f"Произошла ошибка: {e}")
         return None
 
+def set_image():
+    img = load_image(url)
+
+    if img:
+        label.config(image=img)
+        label.image = img
+
+
 window = Tk()
 window.title("Cats!")
 window.geometry("600x480")
@@ -24,11 +32,11 @@ window.geometry("600x480")
 label = Label()
 label.pack()
 
-url = "http://cataas.com/cat"
-img = load_image(url)
+update_button = Button(text="Обновить", command=set_image)
+update_button.pack()
 
-if img:
-    label.config(image=img)
-    label.image = img
+url = "http://cataas.com/cat"
+
+set_image()
 
 window.mainloop()
