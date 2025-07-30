32322ef6a07b84b487190866658c747645fcc180
KseniaYantilina <walxenia@yandex.ru>
2025-07-31 02:20:06 +0500

    Добавлена кнопка загрузки случайного котика

--- Cataas.py
+++ Cataas.py
@@ -6,7 +6,8 @@ from PIL import Image, ImageTk
 import requests
 from io import BytesIO
 
-Allowed_tags = ["sleep","jump","fight","black","white","bengal","siamese","cute","red"]
+Allowed_tags = ["sleep","jump","fight","black","white","bengal",
+                "siamese","cute","red","big","green", "pink", "newyear", "lol"]
 
 def load_image(url):
     try:
@@ -33,13 +34,25 @@ def open_new_window():
         label.pack()
         label.image = img
 
+def open_new_window2():
+    tag = tag_combobox.get()
+    url_tag = "http://cataas.com/cat"
+    img = load_image(url_tag)
+
+    if img:
+        new_window = Toplevel()
+        new_window.title("Картинка котика")
+        new_window.geometry("600x480")
+        label = Label(new_window, image=img)
+        label.pack()
+        label.image = img
 
 def exit():
     window.destroy()
 
 window = Tk()
 window.title("Cats!")
-window.geometry("600x520")
+window.geometry("600x200")
 
 menu_bar = Menu(window)
 window.config(menu=menu_bar)
@@ -52,13 +65,24 @@ file_menu.add_command(label="Выход", command=exit)
 
 url = "http://cataas.com/cat"
 
-tag_label = Label(text="Выбери тег")
-tag_label.pack()
+frame1 = Frame(window, bg="lightgray", padx=10, pady=10)
+frame1.pack()
 
-tag_combobox = ttk.Combobox(values=Allowed_tags)
-tag_combobox.pack()
+frame2 = Frame(window, bg="lightgray", padx=10, pady=10)
+frame2.pack()
 
-load_button = Button(text="Загрузить по тегу", command=open_new_window)
-load_button.pack()
+tag_label = Label(frame1, text="Выбери тег")
+tag_label.pack(padx=10, side="left")
+
+tag_combobox = ttk.Combobox(frame1, values=Allowed_tags)
+tag_combobox.pack(padx=10, side="left")
+
+load_button = Button(frame1, text="Загрузить по тегу", command=open_new_window)
+load_button.pack(side="right")
+
+load_button2 = Button(frame2, text="Загрузить случайного котика", command=open_new_window2)
+load_button2.pack(side="bottom")
+
+
 
 window.mainloop()
